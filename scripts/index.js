dcc430b5c8441ceac9c6f88e071276026ebb62cd
Mikedev <mikeiaspires23@gmail.com>
2022-11-07 13:56:43 -0300

    segundo commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,44 @@
 let sounds = []
 
+function onSound(index) {
+    const pathSound = sounds[index].path
+     
+    const sound = new  Audio(pathSound)
+
+    sound.play()
+
+    console.log(sound)
+}
+
 fetch('../sounds.json').then(response => response.json()).then(data => 
     {sounds = data
-        sounds.map(sound => 
-        console.log(sound)    
-        )     
+        
+        const elementSound = document.querySelector('.sounds')
+
+        sounds.map((sound, index) =>{
+            const li = document.createElement('li')
+            const button = document.createElement('button')
+
+            const label = document.createElement('label')
+            label.innerHTML = sound.label
+            button.appendChild(label)
+
+            const span = document.createElement('span')
+            span.innerHTML = sound.hotkey
+            button.appendChild(span)
+
+            button.setAttribute('onclick', `onSound(${index})`)
+
+
+            console.log(sound)    
+
+         li.appendChild(button)
+
+         elementSound.appendChild(li)
+
+
+
+         } )     
     
     }
 
